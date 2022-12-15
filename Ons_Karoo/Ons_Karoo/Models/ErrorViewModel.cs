4dfc1934a212d765a0be49773ce2d763955e4ae1
Chetan Bodke <chetanbodke29@gmail.com>
2022-12-15 21:12:38 +0530

    first commit

---
+++ Ons_Karoo/Ons_Karoo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ons_Karoo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Ons_KarooCMS/Ons_KarooCMS/Ons_KarooCMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ons_KarooCMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
